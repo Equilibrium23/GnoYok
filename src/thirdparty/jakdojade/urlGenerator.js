a0311a41a736c173de05d7df926dd26bdd0599fa
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2022-11-19 19:37:32 +0100

    Add JakDojade support to Map

--- src/thirdparty/jakdojade/urlGenerator.js
+++ src/thirdparty/jakdojade/urlGenerator.js
@@ -1,13 +1,6 @@
-function generateJakDojadeUrl(town, fromCoords, toCoords, date, time){
-    return `
-        https://jakdojade.pl/${town}/trasa/z----do--
-        ?fc=${fromCoords}
-        &tc=${toCoords}
-        &ft=LOCATION_TYPE_COORDINATE
-        &tt=LOCATION_TYPE_COORDINATE
-        &d=${date}
-        &h=${time}
-        &aro=0
-        &t=1
-    `
+export function generateJakDojadeUrl(town, fromCoords, toCoords){
+    const currentDateTime = new Date();
+    const currentDate = `${currentDateTime.getDate()}.${currentDateTime.getMonth()}.${currentDateTime.getFullYear()}`
+    const currentTime = currentDateTime.toISOString().substr(11,5)
+    return `https://jakdojade.pl/${town}/trasa/z----do--?fc=${fromCoords.toString().replaceAll(",",":")}&tc=${toCoords.toString().replaceAll(",",":")}&ft=LOCATION_TYPE_COORDINATE&tt=LOCATION_TYPE_COORDINATE&d=${currentDate}&h=${currentTime}&aro=0&t=1`
 }
