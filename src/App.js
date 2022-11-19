e3818852ba044af199b4acdb2f4c800466163ab2
Pawel Maksimek <pawelmaksimek@gmail.com>
2022-11-19 18:36:28 +0100

    added filterProductsByRadius & current location

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import { MapContainer } from 'react-leaflet/MapContainer'
-import { Marker, Popup, TileLayer } from "react-leaflet";
+import { Circle, Marker, Popup, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from 'leaflet';
 import MapMarker from "./components/map/MapMarker";
 import sample_data from "./sample_data/sample_data.json"
+import { useEffect, useState } from 'react';
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -35,20 +36,70 @@ function getCoordinates(address) {
         return null
 }
 
+//filterProductsByRadius(centerPos, radius, product.coords)
+function filterProductsByRadius(centerPos, radius, coords) {
+    if (radius == null)
+        return false;
+
+    const DEGREE_2_METER_RATIO = 111139;
+
+    var rad = Math.sqrt(
+        Math.pow(centerPos[0] - coords[0], 2) +
+        Math.pow(centerPos[1] - coords[1], 2)) * DEGREE_2_METER_RATIO;
+
+    return rad <= radius;
+}
+
+const greenOptions = { color: '#94C973' }
+
+const fetchUserLocationOptions = {
+    enableHighAccuracy: true,
+    timeout: 6000,
+    maximumAge: 0
+};
+
+function error(err) {
+    console.warn(`Error during fetching your location: ERROR(${err.code}): ${err.message}`);
+}
 
 function App() {
-    const centerPos = [50.049, 19.94]
-  return (
-      <MapContainer center={centerPos} zoom={13} scrollWheelZoom={true}>
-          <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          {sample_data.map(product => (
-              <MapMarker product={product}/>
-          ))}
-      </MapContainer>
-  );
+    var radius = 2600; //TODO: configurable in Search interface
+
+    const [centerPos, setCenterPos] = useState([50.090786, 19.988419])
+
+    function success(pos) {
+        const crd = pos.coords;
+
+        console.log('Your current position is:');
+        console.log(`Latitude : ${crd.latitude}`);
+        console.log(`Longitude: ${crd.longitude}`);
+        console.log(`More or less ${crd.accuracy} meters.`);
+
+        setCenterPos([crd.latitude, crd.longitude])
+    }
+
+    useEffect(() => {
+        navigator.geolocation.getCurrentPosition(success, error, fetchUserLocationOptions);
+    }, []);
+
+    return (
+        <MapContainer center={centerPos} zoom={13} scrollWheelZoom={true}>
+            <TileLayer
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+
+            <Circle center={centerPos} pathOptions={greenOptions} radius={radius} />
+            <Marker position={centerPos} >
+                <Popup>
+                    This me!
+                </Popup>
+            </Marker>
+            {sample_data.map(product => (
+                <MapMarker product={product} />
+            ))}
+        </MapContainer>
+    );
 }
 
 export default App;
