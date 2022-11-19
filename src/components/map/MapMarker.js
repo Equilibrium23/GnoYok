36b16a16214c4d7f9377dd2e81e16da3245c40eb
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2022-11-19 16:03:29 +0100

    Add new features to custom mapMarker

--- src/components/map/MapMarker.js
+++ src/components/map/MapMarker.js
@@ -1,15 +1,32 @@
 import {Marker, Popup} from "react-leaflet";
-
+import TitleIcon from '@mui/icons-material/Title';
+import DescriptionIcon from '@mui/icons-material/Description';
+import CategoryIcon from '@mui/icons-material/Category';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import ManIcon from '@mui/icons-material/Man';
+import {Box, Stack} from "@mui/material";
 
 function MapMarker(props) {
     return (
-        <Marker position={props.markerPosition}>
+        <Marker position={props.product.coords}>
             <Popup>
-                {props.category} <br/>
-                {props.title} <br/>
-                {props.description} <br/>
-                {props.adress} <br/>
-                {props.owner} <br/>
+                <Stack spacing={2}>
+                    <Box>
+                       <CategoryIcon/> {props.product.category}
+                    </Box>
+                    <Box>
+                       <TitleIcon/> {props.product.title}
+                    </Box>
+                    <Box>
+                      <DescriptionIcon/>  {props.product.description}
+                    </Box>
+                    <Box>
+                      <LocationOnIcon/>  {props.product.adress}
+                    </Box>
+                    <Box>
+                       <ManIcon/> {props.product.owner}
+                    </Box>
+                </Stack>
             </Popup>
         </Marker>
     );
