df4a80f5c7cb03a37f1bfa25ddee440004b4f4e6
Equilibrium23 <wojciech.jedraski1999@gmail.com>
2022-11-19 16:03:29 +0100

    Add new features to custom mapMarker

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import {Marker, Popup, TileLayer} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from 'leaflet';
 import MapMarker from "./components/map/MapMarker";
+import sample_data from "./sample_data/sample_data.json"
 
 delete L.Icon.Default.prototype._getIconUrl;
 
@@ -16,21 +17,14 @@ L.Icon.Default.mergeOptions({
 
 function App() {
     const centerPos = [50.049, 19.94]
-    const positions = [
-        [50.049, 19.94],
-        [50.149, 19.94],
-        [50.249, 19.94],
-        [50.099, 19.94],
-    ];
   return (
       <MapContainer center={centerPos} zoom={13} scrollWheelZoom={true}>
           <TileLayer
               attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
               url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          {positions.map(markerPosition => (
-              <MapMarker markerPosition={markerPosition} description={"XD"}>
-              </MapMarker>
+          {sample_data.map(product => (
+              <MapMarker product={product}/>
           ))}
       </MapContainer>
   );
