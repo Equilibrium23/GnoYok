fc1d9d88eaf6d9b7d5e828d9bf5370db4464c09c
talik <carykpawel@gmail.com>
2022-11-19 13:20:09 +0100

    Update adding product

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react'
+import { FormControl, InputLabel, Input, FormHelperText } from '@mui/material';
 
 export default class AddProduct extends Component {
-    constructor(props) {
-        super(props);
-        this.state = props.products;
-    }
-    
+  
   render() {
     return (
-      <div>AddProduct</div>
+    <FormControl>
+      <InputLabel htmlFor="my-input">Email address</InputLabel>
+      <Input id="my-input" aria-describedby="my-helper-text" />
+      <FormHelperText id="my-helper-text">We'll never share your email.</FormHelperText>
+    </FormControl>
     )
   }
 }
