a837cf443f405a8f7e4da43c9b629411d89ccb34
fdeods <fdeods@gmail.com>
2022-11-19 17:56:37 +0100

    pretty labels

--- src/AddProduct.js
+++ src/AddProduct.js
@@ -1,8 +1,8 @@
 import { useForm, Controller } from "react-hook-form";
-import { Input, Container, Button } from "@mui/material";
+import { InputLabel, Input, Container, Button } from "@mui/material";
 
 export default function AddProduct() {
-  //{ category: "", title: "", description: "", adress: "", coords: "", owner: ""}
+  
   const { handleSubmit, control } = useForm({
     defaultValues: {
       category: '',
@@ -16,38 +16,48 @@ export default function AddProduct() {
   const onSubmit = data => console.log(data);
 
   return (
-    <Container maxWidth="sm">
+    <Container maxWidth="md">
       <form onSubmit={handleSubmit(onSubmit)}>
+        <InputLabel htmlFor="category-input">Category</InputLabel>
         <Controller
           name="category"
           control={control}
           rules={{ required: true }}
-          render={({ field }) => <Input {...field} />}
+          render={({ field }) => <Input id="category-input" aria-describedby="category-input-text" {...field} />}
         />
+
+        <InputLabel htmlFor="title-input">Title</InputLabel>
         <Controller
           name="title"
           control={control}
           rules={{ required: true }}
-          render={({ field }) => <Input {...field} />}
+          render={({ field }) => <Input id="title-input" aria-describedby="title-input-text" {...field} />}
         />
-        <Controller
-          name="description"
-          control={control}
-          rules={{ required: true }}
-          render={({ field }) => <Input {...field} />}
-        />
+
+        <InputLabel htmlFor="adress-input">Address</InputLabel>
         <Controller
           name="adress"
           control={control}
           rules={{ required: true }}
-          render={({ field }) => <Input {...field} />}
+          render={({ field }) => <Input id="adress-input" aria-describedby="adress-input-text"{...field} />}
         />
+
+        <InputLabel htmlFor="owner-input">Owner</InputLabel>
         <Controller
           name="owner"
           control={control}
           rules={{ required: true }}
-          render={({ field }) => <Input {...field} />}
+          render={({ field }) => <Input id="owner-input" aria-describedby="owner-input-text"{...field} />}
         />
+
+        <InputLabel htmlFor="description-input">Description</InputLabel>
+        <Controller
+          name="description"
+          control={control}
+          rules={{ required: true }}
+          render={({ field }) => <Input id="description-input" multiline="true" aria-describedby="description-input-text" {...field} />}
+        />
+
         <Button type="submit">Submit</Button>
       </form>
     </Container>
